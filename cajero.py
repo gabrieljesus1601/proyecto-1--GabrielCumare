4f24b15124f95cb232ee25bb159d1c3da75b6698
Gabriel Cumare <55671412+gabrieljesus1601@users.noreply.github.com>
2022-06-09 07:55:40 -0400

    Update cajero.py

--- cajero.py
+++ cajero.py
@@ -2,35 +2,35 @@
 #ejercicio 3 del proyecto 
 """ programa que simula un cajero automatico que solicita al usuario el monto en dolares que desea retirar con la cantidad minima de billetes """
 try:
-    money = int(input("introduzca el monto que desea retirar: "))
-    cashList = [] #lista donde que ira descontando y dando los billetes
-    if money < 0: 
+    dinero = int(input("introduzca el monto que desea retirar: "))
+    lista_dinero = [] #lista donde que ira descontando y dando los billetes
+    if dinero < 0: 
         print("este monto no es posible de retirar") # el monto a retirar no puede ser negativo
     else:
-        while money > 0: 
+        while dinero > 0: 
 #condicionales para que se use la menor cantidad de billetes
-            if (money >= 100):
-                cashList.append(100) 
-                money -= 100
-            elif (money >= 50):
-                cashList.append(50)
-                money -= 50
-            elif (money >= 20):
-                cashList.append(20)
-                money -= 20
-            elif (money >= 10):
-                cashList.append(10)
-                money -= 10
-            elif (money >= 5):
-                cashList.append(5)
-                money -= 5
-            elif (money >= 1):
-                cashList.append(1)
-                money -= 1
+            if (dinero >= 100):
+                lista_dinero.append(100) 
+                dinero -= 100
+            elif (dinero >= 50):
+                lista_dinero.append(50)
+                dinero -= 50
+            elif (dinero >= 20):
+                lista_dinero.append(20)
+                dinero -= 20
+            elif (dinero >= 10):
+                lista_dinero.append(10)
+                dinero -= 10
+            elif (dinero >= 5):
+                lista_dinero.append(5)
+                dinero -= 5
+            elif (dinero >= 1):
+                lista_dinero.append(1)
+                dinero -= 1
                 
-        print("denominacion del billete a entregar : billetes a etregar") 
+        print("billete de : billetes a etregar") 
 # crear un diccionario que cuenta  la cantidad de veces que aparecen los billetes
-        result = dict((cash, cashList.count(cash)) for cash in cashList)
+        result = dict((dinero, lista_dinero.count(dinero)) for dinero in lista_dinero)
         print(result)
 except:
     print("este valor no es valido") # el monto a retirar no puede ser de otro tipo que no sea entero
