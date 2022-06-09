a4a4f7aefc1528d5e105f9682947492251f96392
Gabriel Cumare <55671412+gabrieljesus1601@users.noreply.github.com>
2022-06-09 08:14:33 -0400

    Update variaciones.py

--- variaciones.py
+++ variaciones.py
@@ -1,9 +1,8 @@
 # proyecto-GabrielCumare
 #ejercio 1 proyecto programacion
 """programa que registra variaciones gravimetricas diarias en tuplas para valores maximos y minimos introducidos por el usuario, indicando cuantos dias duro la salida, cantidad de errores y la variacion media minima y maxima   """
-
-#Creo una función que reconoce los máximos y mínimos.
 def checkTuple(temp):
+    """ funcion que reconoce los maximos y minimos"""
     if temp[0] < 0 or temp[0] > 50:
         return 0
     elif temp[1] < 0 or temp[1] > 50:
@@ -25,48 +24,48 @@ if __name__ == '__main__':
             break
     #Defino los contadores y variables
 
-    dayCount = 0
-    errorCount = 0
-    menosCien = 0 
-    masCien = 0
-    minimum = 50 
-    maximum = 0
+    dia_contador = 0
+    error_contador = 0
+    menos_cien = 0 
+    mas_cien = 0
+    minimo = 50 
+    maximo = 0
     
     #Creo un ciclo que me recorrerá los días, y estudiarán los datos.
     
-    for element in lista:
-        dayCount += 1
+    for elemento in lista:
+        dia_contador += 1
         #Verifico que el dato no sea un error de entrada.
-        if checkTuple(element) == False:
-            errorCount += 1
+        if checkTuple(elemento) == False:
+            error_contador += 1
         #Hago el conteo de los -100 y 100
-        if element[0] < -100:
-            menosCien += 1
-        elif element[0] > 100:
-            masCien += 1
-        if element[1] < -100:
-            menosCien += 1
-        elif element[1] > 100:
-            masCien += 1
+        if elemento[0] < -100:
+            menos_cien += 1
+        elif elemento[0] > 100:
+            mas_cien += 1
+        if elemento[1] < -100:
+            menos_cien += 1
+        elif elemento[1] > 100:
+            mas_cien += 1
 
         #Verifico que los elementos estén entre 0 y 50, para los máximos y mínimos.
-        if 0 <= element[0] <= 50 and 0 <= element[1] <= 50:
-            if element[0] > maximum:
-                maximum = element[0]
-            elif element[1] > maximum:
-                maximum = element[1]
+        if 0 <= elemento[0] <= 50 and 0 <= elemento[1] <= 50:
+            if elemento[0] > maximo:
+                maximo = elemento[0]
+            elif elemento[1] > maximo:
+                maximo = elemento[1]
             
-            if element[0] < minimum:
-                minimum = element[0]
-            elif element[1] < minimum:
-                minimum = element[1]
+            if elemento[0] < minimo:
+                minimo = elemento[0]
+            elif elemento[1] < minimo:
+                minimo = elemento[1]
                 
         #Imprimo los valores en pantalla
-print('Total dias: {}'.format(dayCount))
-print('Total errores: {}'.format(errorCount))
-print('Total errores < -100 (menores a 100) : {}'.format( menosCien))
-print('Total errores > 100 (mayores a 100): {}'.format(masCien))
-print('Total errores variaciones de 100: {}'.format(menosCien + masCien))
-print('Minimo: {}'.format(minimum))
-print('Maximo: {}'.format(maximum))
+print('Total dias: {}'.format(dia_contador))
+print('Total errores: {}'.format(error_contador))
+print('Total errores < -100 (menores a 100) : {}'.format( menos_cien))
+print('Total errores > 100 (mayores a 100): {}'.format(mas_cien))
+print('Total errores variaciones de 100: {}'.format(menos_cien + mas_cien))
+print('Minimo: {}'.format(minimo))
+print('Maximo: {}'.format(maximo))
             
